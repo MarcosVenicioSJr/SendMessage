e29172ee7758ddfc13b6963a555cc8236e0e927b
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-07-21 09:01:45 -0300

    Add configurantion TWILIO and creating Models Message, Ordering

---
+++ Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace SendMessage.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Messages { get; set; }
+    }
+}
